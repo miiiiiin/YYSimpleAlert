c5a9db62fa8829a912406a97845231323412e309
songkyung <min.songkyung@gmail.com>
2021-01-22 18:05:39 +0900

    set YYSimpleViewController

--- Example/YYSimpleAlert/ViewController.swift
+++ Example/YYSimpleAlert/ViewController.swift
@@ -9,13 +9,8 @@
 import UIKit
 import YYSimpleAlert
 
-class ViewController: UIViewController {
-    
-    let simpleVC = YYSimpleViewController()
-    open lazy var items = YYSimple.items
-    
-    @IBOutlet weak var tableView: UITableView!
-    
+class ViewController: YYSimpleViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,8 +18,8 @@ class ViewController: UIViewController {
         print("items check: \(items)")
         var item = ViewItem(name: "YYSimple")
         item.detailText = "The MIT License (MIT) ...Very long text..."
-        simpleVC.items.append(item)
-        simpleVC.items.sort { $0.name < $1.name }
+//        simpleVC.items.append(item)
+//        simpleVC.items.sort { $0.name < $1.name }
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,38 +37,8 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+extension ViewController {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
     }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SimpleCell", for: indexPath) as! SimpleCell
-        print("simplecell check: \(cell), \(items.count)")
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 52
-    }
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = table.dequeueReusableCell(withIdentifier: "LicenseCell", for: indexPath) as! LicenseCell
-//
-//        let item = self.items[indexPath.row]
-//        cell.licenseLbl?.text = item.displayName
-//        cell.detailTextLabel?.text = item.licenseName
-//        cell.accessoryType = .disclosureIndicator
-//
-//        return cell
-//    }
-    
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        tableView.deselectRow(at: indexPath, animated: false)
-//        let carteItem = self.items[indexPath.row]
-//        let controller = UIStoryboard.storyboard(.settings).instantiateViewController(withIdentifier: "OpenSourceDetailViewController") as! OpenSourceDetailViewController
-//        controller.item = carteItem
-//        self.navigationController?.pushViewController(controller, animated: true)
-//    }
-//
 }
